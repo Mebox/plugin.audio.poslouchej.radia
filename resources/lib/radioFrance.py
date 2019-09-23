09f48c745d1937d45e809913497a88b289afa772
Mebox <47082726+Mebox@users.noreply.github.com>
2019-09-23 18:39:05 +0200

    Update radioFrance.py

--- resources/lib/radioFrance.py
+++ resources/lib/radioFrance.py
@@ -31,8 +31,7 @@ class Main:
         self = selfGet
     
         list = [
-            ['B4B Radio Club Dance', 'http://b4bsecours2.radioca.st/8727/', 'https://i46.servimg.com/u/f46/19/40/01/67/c17559.png'],
-            ['Feeling Floyd Rock', 'http://195.154.180.106:8032/stream', 'https://i46.servimg.com/u/f46/19/40/01/67/c17568.png'],
+            ['B4B Radio Club Dance', 'https://radio10.pro-fhi.net/radio/9000/stream', 'https://i46.servimg.com/u/f46/19/40/01/67/c17559.png'],
             ['E-Dance 90s', 'http://94.23.221.158:9197/stream', 'https://i46.servimg.com/u/f46/19/40/01/67/c17567.png'],
             ['Hotmixradio 80', 'http://streaming.hotmixradio.fm/hotmixradio-80-128.mp3', 'https://i46.servimg.com/u/f46/19/40/01/67/c17560.png'],
             ['Hotmixradio FUNKY', 'http://streaming.hotmixradio.fm/hotmixradio-funky-128.mp3', 'https://i46.servimg.com/u/f46/19/40/01/67/c17563.png'],
@@ -40,8 +39,7 @@ class Main:
             ['Hotmixradio HITS', 'http://streaming.hotmixradio.fm/hotmixradio-hits-128.mp3', 'https://i46.servimg.com/u/f46/19/40/01/67/c17572.png'],
             ['Hotmixradio 2000', 'http://streaming.hotmixradio.fm/hotmixradio-2k-128.mp3', 'https://i46.servimg.com/u/f46/19/40/01/67/c17573.png'],
             ['Hotmixradio HOT', 'http://streaming.hotmixradio.fm/hotmixradio-hot-128.mp3', 'https://i46.servimg.com/u/f46/19/40/01/67/c17574.png'],
-            ['Nostalgie Rock', 'http://cdn.nrjaudio.fm/adwz1/fr/30621/mp3_128.mp3?origine=radio.net', 'https://i46.servimg.com/u/f46/19/40/01/67/c17561.png'],
-            ['NRJ France', 'http://185.52.127.155/fr/30001/mp3_128.mp3?origine=radio.net', 'https://i46.servimg.com/u/f46/19/40/01/67/c17570.png'],
+            ['NRJ France', 'http://185.52.127.168/fr/30001/mp3_128.mp3', 'https://i46.servimg.com/u/f46/19/40/01/67/c17570.png'],
             ['Puls Radio - Dance', 'http://icecast.pulsradio.com:80/pulsHD.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/pulsra10.jpg'],
             ['Puls Radio - Lounge', 'http://icecast.pulsradio.com/relaxHD.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/lounfe10.jpg'],
             ['Roots Legacy Radio', 'http://rootslegacy.fr:8080/stream/1/', 'https://i46.servimg.com/u/f46/19/40/01/67/c17566.png'],
