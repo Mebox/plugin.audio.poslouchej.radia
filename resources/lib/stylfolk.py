40ebf8aa7080e744150447cd4a7500ef9c16d54f
Mebox <47082726+Mebox@users.noreply.github.com>
2019-09-24 16:21:45 +0200

    Update stylfolk.py

--- resources/lib/stylfolk.py
+++ resources/lib/stylfolk.py
@@ -32,11 +32,9 @@ class Main:
     
         list = [
             ['Country Rádio (CZ)', 'http://icecast4.play.cz/country128.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radioc33.jpg'],
-            ['Rádio 66 (CZ)', 'http://ice3.abradio.cz/route128.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radio610.jpg'],
             ['Rádio Country (CZ)', 'http://mp3stream4.abradio.cz/country128.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radioc14.jpg'],
             ['Rádio Folk (CZ)', 'http://mp3stream2.abradio.cz/folk128.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radiof15.jpg'],
             ['Rádio Dálnice (CZ)', 'http://icecast8.play.cz/radiodalnice.mp3', 'https://i46.servimg.com/u/f46/19/40/01/67/radio-10.png'],
-            ['Rádio Petrov Folk (CZ)', 'http://play.radiopetrov.com:8000/petrovFC', 'https://i62.servimg.com/u/f62/19/40/01/67/radiop14.jpg'],
             ['Rádio Samson FM (CZ)', 'http://icecast8.play.cz/samsonfm128.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radios17.jpg'],
             ['181.fm 90s Country (USA)', 'http://uplink.duplexfx.com:8050', 'https://i46.servimg.com/u/f46/19/40/01/67/c175125.png'],
             ['181.fm Real Country (USA)', 'http://relay.181.fm:8034', 'https://i46.servimg.com/u/f46/19/40/01/67/c175125.png']
