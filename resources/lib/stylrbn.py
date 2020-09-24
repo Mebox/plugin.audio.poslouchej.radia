9cd15e0ddb9a83bc8a087f737476a6a4bdacc6b6
Mebox <47082726+Mebox@users.noreply.github.com>
2020-09-24 13:59:51 +0200

    Update stylrbn.py

--- resources/lib/stylrbn.py
+++ resources/lib/stylrbn.py
@@ -31,9 +31,8 @@ class Main:
         self = selfGet
     
         list = [
-            ['tvojeRADIO.com Hip-Hop (SK)', 'http://server1.internetoveradio.sk:8816/hiphop.mp3', 'https://i11.servimg.com/u/f11/19/40/01/67/radioh10.jpg'],
             ['COLOR Music Radio (CZ)', 'http://icecast6.play.cz/color192.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radioc19.jpg'],
-            ['Funkstar Radio (CZ)', 'http://167.114.64.181:8698/stream', 'https://i46.servimg.com/u/f46/19/40/01/67/funkst10.jpg'],
+            ['Funkstar Radio (CZ)', 'https://funkstar.radioca.st/stream?ver=153586', 'https://i46.servimg.com/u/f46/19/40/01/67/funkst10.jpg'],
             ['HipHopVibes Rádio (CZ)', 'http://mp3stream4.abradio.cz/hiphopvibes128.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radioh14.jpg'],
             ['Rádio Spin 96,2 FM (CZ)', 'http://icecast4.play.cz/spin128.mp3', 'https://i62.servimg.com/u/f62/19/40/01/67/radios14.jpg'],
             ['Radio City (BG)', 'http://79.98.108.172:8022/', 'https://i62.servimg.com/u/f62/19/40/01/67/radioc36.jpg'],
